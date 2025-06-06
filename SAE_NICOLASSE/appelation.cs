912396d77bfff8efda341b770190b0db13284aa1
Arthur Lopes <Arthur.Lopes@etu.univ-smb.fr>
2025-06-06 09:09:31 +0200

    première class

---
+++ SAE_NICOLASSE/appelation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE_NICOLASSE
+{
+    public class appelation
+    {
+
+        private int numappelation;
+        private string nomappelation;
+
+        public int Numappelation
+        {
+            get
+            {
+                return this.numappelation;
+            }
+
+            set
+            {
+                this.numappelation = value;
+            }
+        }
+
+        public string Nomappelation
+        {
+            get
+            {
+                return this.nomappelation;
+            }
+
+            set
+            {
+                this.nomappelation = value;
+            }
+        }
+    }
+}
