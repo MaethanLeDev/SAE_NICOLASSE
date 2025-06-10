b7788cdeef97966ce1ec36b757b413c87aeca64e
Arthur Lopes <Arthur.Lopes@etu.univ-smb.fr>
2025-06-10 09:46:55 +0200

    Debut du wpf avec les users controls

--- SAE_NICOLASSE/MainWindow.xaml.cs
+++ SAE_NICOLASSE/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SAE_NICOLASSE.UserControls;
 
 namespace SAE_NICOLASSE
 {
@@ -19,6 +20,21 @@ namespace SAE_NICOLASSE
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new UCListeVin();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new UCDemande();
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new UCCommande();
         }
     }
 }
