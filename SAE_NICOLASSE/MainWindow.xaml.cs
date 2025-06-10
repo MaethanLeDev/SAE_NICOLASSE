f49aced2ccc1c81fc28b458de272ce39ac831af2
Maethan Tordeux <Maethan.Tordeux@etu.univ-smb.fr>
2025-06-10 16:31:36 +0200

    TresBelleVoic

--- SAE_NICOLASSE/MainWindow.xaml.cs
+++ SAE_NICOLASSE/MainWindow.xaml.cs
@@ -1,24 +1,10 @@
 ﻿using Npgsql;
 using System.Data;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using SAE_NICOLASSE.UserControls;
-
-
 
 namespace SAE_NICOLASSE
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -27,13 +13,15 @@ namespace SAE_NICOLASSE
             LoadVinData();
         }
 
-        private void LoadVinData() // <-- Cette méthode doit être à l'intérieur de la classe MainWindow
+        private void LoadVinData()
         {
             try
             {
-                var cmd = new NpgsqlCommand("SELECT * FROM Vin"); // Assure-toi que la table Vin existe
+                var cmd = new NpgsqlCommand("SELECT * FROM Vin");
                 DataTable dt = DataAccess.Instance.ExecuteSelect(cmd);
-                
+
+                // Ici vous pouvez utiliser dt pour alimenter votre ItemsControl
+                // WineItemsControl.ItemsSource = dt.DefaultView;
             }
             catch (Exception ex)
             {
@@ -41,6 +29,7 @@ namespace SAE_NICOLASSE
             }
         }
 
+        // Gérer les clics sur les boutons de navigation
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
             MainContent.Content = new UCListeVin();
